b47838d48e245f4043e4bedb7c0a22f93792e5f0
Dravin <100250406+drickpdr98@users.noreply.github.com>
2023-03-30 21:16:42 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -140,6 +140,20 @@ int main()
   letterCounter(str,chr);
 
   }
+  
+  
+  
+  #include <map>
+#include <string>
+using namespace std;
+
+int main() {
+  map<int, string> sample_map;
+  sample_map.insert(pair<int, string>(1, "one"));
+  sample_map.insert(pair<int, string>(2, "two"));
+
+  cout << sample_map[1] << " " << sample_map[2] << endl;
+}
   catch(string &error)
   {
   cerr <<error<< endl;
