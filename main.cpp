6d60a58a449ccb0d9be5550349407769f1ce54d8
Dravin <100250406+drickpdr98@users.noreply.github.com>
2023-04-29 10:47:29 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,81 @@
 #include <iostream>
 #include<time.h>
-#include<cctype>
+#include <fstream>
+#include <string>
 
 using namespace std;
-// Create a function, named load_encrypt_array, that loads an 26 Capacity Array, named encrypt_offset, with Randon Numbers from 0 to 100.
 
-void load_encrypt_array(int arr[], size_t size)
-{
-    cout << "Encrypted Offset: " << endl;
-    for (int i = 0; i < size; i++)
-    {
-        arr[i] = rand() % (100 - 0 + 1) + 0;
-        cout << arr[i] << " ";
+int main() {
+    ifstream input("input.txt");  
+
+    ofstream output("output.txt"); 
+
+    string record;
+
+    while (getline(input, record)) { 
+        output << record << endl; 
     }
-}
 
-// Create another function, named encrypt_string, that passes the preceeding array,a All Upper Case input string and a Output String.
+    input.close();
+    output.close();
 
-void encrypt_string(string str, int encrypt_offset[])
-{
-    string Output_String;
-
-    cout << "Encrypted String: ";
-
-    for (int i = 0; i < str.length(); i = i + 1)
-    {
-        //convert if not uppercase
-        toupper(str[i]);
-
-        char encrypt_Char = static_cast<char>((static_cast<int>(str[i]) + encrypt_offset[static_cast<int>(str[i]) - 65]) % 26 + 65);
-        Output_String += encrypt_Char;
-	}
-
-    cout << Output_String<< endl;
-
-}
-
-int main()
-{
-    srand(time(NULL));
-    const int CAPACITY = 26;
-
-    cout << "Enter a string to encrypt > " << endl;
-    string str;
-    cin >> str;
-
-    cout << endl;
-
-    int encrypt_offset[CAPACITY] = {};
-
-    cout << "Original String: " << str << endl;
-  
-    encrypt_string(str, encrypt_offset);
-
-    load_encrypt_array(encrypt_offset, CAPACITY);
-    
     return 0;
 }
+
+// // Create a function, named load_encrypt_array, that loads an 26 Capacity Array, named encrypt_offset, with Randon Numbers from 0 to 100.
+
+// void load_encrypt_array(int arr[], size_t size)
+// {
+//     cout << "Encrypted Offset: " << endl;
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rand() % (100 - 0 + 1) + 0;
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// // Create another function, named encrypt_string, that passes the preceeding array,a All Upper Case input string and a Output String.
+
+// void encrypt_string(string str, int encrypt_offset[])
+// {
+//     string Output_String;
+
+//     cout << "Encrypted String: ";
+
+//     for (int i = 0; i < str.length(); i = i + 1)
+//     {
+//         //convert if not uppercase
+//         toupper(str[i]);
+
+//         char encrypt_Char = static_cast<char>((static_cast<int>(str[i]) + encrypt_offset[static_cast<int>(str[i]) - 65]) % 26 + 65);
+//         Output_String += encrypt_Char;
+// 	}
+
+//     cout << Output_String<< endl;
+
+// }
+
+// int main()
+// {
+//     srand(time(NULL));
+//     const int CAPACITY = 26;
+
+//     cout << "Enter a string to encrypt > " << endl;
+//     string str;
+//     cin >> str;
+
+//     cout << endl;
+
+//     int encrypt_offset[CAPACITY] = {};
+
+//     cout << "Original String: " << str << endl;
+  
+//     encrypt_string(str, encrypt_offset);
+
+//     load_encrypt_array(encrypt_offset, CAPACITY);
+    
+//     return 0;
+// }
 
 // void printGreaterThanX(int arr[],int value, size_t size)
 // {
